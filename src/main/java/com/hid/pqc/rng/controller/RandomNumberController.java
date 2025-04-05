b666ce124156b47721d882f4fef40a626854ffa7
vipaul <Vinmar@2025>
2025-04-05 23:24:26 +0530

    Add test  html

--- src/main/java/com/hid/pqc/rng/controller/RandomNumberController.java
+++ src/main/java/com/hid/pqc/rng/controller/RandomNumberController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hid.pqc.rng.model.BraketProgram;
 import com.hid.pqc.rng.processor.QuantumJobProcessor;
 import com.hid.pqc.rng.processor.QuantumMultipleJobProcessor;
+import com.hid.pqc.rng.processor.RandomNumberGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +16,7 @@ import software.amazon.awssdk.services.braket.model.*;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -50,29 +52,68 @@ public class RandomNumberController {
     public RandomNumberController() {
         this.braketClient = BraketClient.create();
     }
-@PostMapping("/quantum/generate/rng")
-public  String  generateQuantumRandomNumbers(@RequestBody BraketProgram braketProgram) throws JsonProcessingException, InterruptedException {
+    /* @PostMapping("/quantum/generate/rng")
+   public  String  generateQuantumRandomNumbers(@RequestBody BraketProgram braketProgram) throws JsonProcessingException, InterruptedException {
 
 
-    return quantumJobProcessor.getQuantumRandomNumber(braketProgram,deviceArn,s3,s3Prefix,braketClient);
-}
+       return quantumJobProcessor.getQuantumRandomNumber(braketProgram,deviceArn,s3,s3Prefix,braketClient);
+   }
 
-    @GetMapping("/quantum/job")
-    public  String  generateQuantumRandomNumbersForJobArn(@RequestParam(name="jobArn") String jobArn) throws JsonProcessingException, InterruptedException {
-log.info(jobArn);
+       @GetMapping("/quantum/job")
+       public  String  generateQuantumRandomNumbersForJobArn(@RequestParam(name="jobArn") String jobArn) throws JsonProcessingException, InterruptedException {
+   log.info(jobArn);
 
-        return  quantumJobProcessor.getQuantumMeasurementResults(jobArn,braketClient);
+           return  quantumJobProcessor.getQuantumMeasurementResults(jobArn,braketClient);
+       }
+     @PostMapping("/sim/run/jobs/{count}")
+       public String runQuantumJobs(@PathVariable(name = "count") int count) {
+           String requestId = UUID.randomUUID().toString();
+           quantumMultipleJobProcessor.processJobs(requestId,count);
+
+           return requestId; // Immediately return requestId
+       }*/
+    @PostMapping("/hexadecimal/{count}")
+    public List<String> runHexaDecimal(@PathVariable(name = "count") int count) {
+        List<String> randomHexList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            randomHexList.add( RandomNumberGenerator.generateRandomHex(24));
+        }
+        return randomHexList;
     }
-    @PostMapping("/sim/run/jobs/{count}")
-    public String runQuantumJobs(@PathVariable int count) {
+    @PostMapping("/hexadecimal/onebyOne/{count}")
+    public List<String> runOneByOneHexaDecimal(@PathVariable(name = "count") int count) {
+        List<String> randomHexList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            randomHexList.add( RandomNumberGenerator.generateRandomHexOneByOne(24));
+        }
+        return randomHexList;
+    }
+
+        @PostMapping("/IonQ/run/jobs/{count}/{shots}")
+    public String runQuantumJobsWithShots(@PathVariable(name = "count") int count,@PathVariable(name = "shots") int shots) {
+        if (count <= 0 || shots <= 0) {
+            throw new IllegalArgumentException("Count and shots must be positive integers");
+        }
         String requestId = UUID.randomUUID().toString();
-        quantumMultipleJobProcessor.processJobs(requestId,count);
+        new QuantumMultipleJobProcessor(braketClient,"arn:aws:braket:us-east-1::device/qpu/ionq/Forte-1",s3,s3Client,s3Prefix,pool,"quantum2.json").processJobs(requestId,count,shots);
 
         return requestId; // Immediately return requestId
     }
 
+    @PostMapping("/sim/run/jobs/{count}/{shots}")
+    public String runQuantumJobsWithShotsSim(@PathVariable(name = "count") int count,@PathVariable(name = "shots") int shots) {
+        if (count <= 0 || shots <= 0) {
+            throw new IllegalArgumentException("Count and shots must be positive integers");
+        }
+        String requestId = UUID.randomUUID().toString();
+        new QuantumMultipleJobProcessor(braketClient,"arn:aws:braket:::device/quantum-simulator/amazon/tn1",s3,s3Client,s3Prefix,pool,"quantum2.json").processJobs(requestId,count,shots);
+
+        return requestId; // Immediately return requestId
+    }
+
+
     @GetMapping("/sim/results/{requestId}")
-    public ResponseEntity<List<Map<String, Object>>> getQuantumResults(@PathVariable String requestId) {
+    public ResponseEntity<List<Map<String, Object>>> getQuantumResults(@PathVariable(name="requestId") String requestId) {
         return quantumMultipleJobProcessor.getQuantumResults(requestId);
 
     }
--- src/main/java/com/hid/pqc/rng/processor/RandomNumberGenerator.java
+++ src/main/java/com/hid/pqc/rng/processor/RandomNumberGenerator.java
@@ -3,13 +3,25 @@ import java.security.SecureRandom;
 
 public class RandomNumberGenerator {
 
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static  SecureRandom secureRandom = new SecureRandom();
 
     public static String generateRandomHex(int length) {
         StringBuilder sb = new StringBuilder(length);
         String characters = "0123456789abcdef";
 
         for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(secureRandom.nextInt(characters.length())));
+        }
+
+        return sb.toString();
+    }
+
+    public static String generateRandomHexOneByOne(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        String characters = "0123456789abcdef";
+
+        for (int i = 0; i < length; i++) {
+            secureRandom = new SecureRandom();
             sb.append(characters.charAt(secureRandom.nextInt(characters.length())));
         }
 
